a72d37147a9c7e37aba75bad5f4b77392b81bcc1
Levi Oliveira dos Santos <leviolisan@gmail.com>
2026-09-01 19:54:22 -0300

    Update cinema.cpp

--- Codes/OBI/cinema.cpp
+++ Codes/OBI/cinema.cpp
@@ -15,20 +15,28 @@ signed main() {
         cin >> T[i];
     for (int j = 0; j < M; j++)
         cin >> H[j];
-  
+
+    // ans[j] guarda quantas pessoas estão atualmente na sessão j.
     vector<int> ans(M, 0);
-    // i = ponteiro para a primeira pessoa que ainda não foi alocada
+    // i é o ponteiro para a primeira pessoa que ainda não foi alocada
     int i = 0;
     // percorremos as sessões em ordem
     for (int j = 0; j < M; j++) {
-        // Enquanto a pessoa puder entrar e ainda houver espaço na sessão
+        // enquanto a pessoa puder entrar e ainda houver espaço na sessão
         while (i < N && T[i] <= H[j] && ans[j] < C) {
             ans[j]++;
             i++;
         }
+        /*
+        Poderíamos por um if da forma
+            if(i == n) break;
+        pois, caso i == n, então todas as pessoas já foram alocadas.
+        */
     }
     for (int j = 0; j < M; j++) {
         cout << ans[j] << ' ';
     }
     return 0;
+    // Complexidade O(N + M)
+    // Se o array não tivesse ordenado, teriamos que ordená-lo, o que 
 }
